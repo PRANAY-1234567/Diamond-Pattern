51b3734a19d7c5cc84967c8b833a3f288056e694
Pranay Jadhao <pranayvjadhao197@gmail.com>
2026-03-05 21:47:19 +0530

    Update diamond.py

--- diamond.py
+++ diamond.py
@@ -1,9 +1,7 @@
 rows = 5
 
-# Upper Part
 for i in range(1, rows + 1):
     print(" " * (rows - i) + "*" * (2*i - 1))
 
-# Lower Part
 for i in range(rows - 1, 0, -1):
     print(" " * (rows - i) + "*" * (2*i - 1))
